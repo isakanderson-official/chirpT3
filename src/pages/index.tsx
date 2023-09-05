cc4e3efd084f55c29a2e3fad5ad9e478759fd3f1
Isak Anderson <isak@isakanderson.com>
2023-09-05 06:53:29 -0700

    Getting data from posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
+  const { data } = api.posts.getAll.useQuery();
   const user = useUser();
   return (
     <>
@@ -20,7 +19,9 @@ export default function Home() {
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
         </div>
-        <SignIn />
+        <div>
+          {data?.map((post) => <div key={post.id}>{post.content}</div>)}
+        </div>
       </main>
     </>
   );
