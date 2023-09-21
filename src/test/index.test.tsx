359d39dbae5b71c8290609b399e1992edc1b3c40
Isak Anderson <isak@isakanderson.com>
2023-09-21 07:28:39 -0700

    Jest setup

---
+++ src/test/index.test.tsx
@@ -0,0 +1,21 @@
+// import React from "react";
+// import { render, screen } from "@testing-library/react";
+// import Home from "~/pages";
+
+// describe("Homepage", () => {
+//   it("renders a heading", () => {
+//     render(<Home />);
+
+//     const heading = screen.getByRole("heading", {
+//       name: /welcome to next\.js!/i,
+//     });
+
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+//     expect(heading).toBeInTheDocument();
+//   });
+
+//   it("renders homepage unchanged", () => {
+//     const { container } = render(<Home />);
+//     expect(container).toMatchSnapshot();
+//   });
+// });
